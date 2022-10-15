99ad08a9a502d0f4fd1603700ad0aa5891f2d770
duybx <1234560ac>
2022-10-15 22:12:18 +0700

    15/10

--- js/selectors.js
+++ js/selectors.js
@@ -3,21 +3,20 @@
 // 2. Current Turn
 // 3. Replay Game
 // 4. Game status
-export function getCellElementList() {
-    return document.querySelectorAll('#cellList > li') ;
+export function getGameStatusElement() {
+    return document.getElementById('gameStatus') ;
 }
-
 export function getCurrentTurnElement() {
     return document.getElementById('currentTurn');
+}
+export function getCellElementList() {
+    return document.querySelectorAll('#cellList > li') ;
 }
 
 export function getCellElementAtIdx(index) {
     return document.querySelector(`#cellList > li:nth-child(${index + 1})`) ;
 }
 
-export function getGameStatusElement() {
-    return document.getElementById('gameStatus') ;
-}
 
 export function getReplayButtonElement(){
     return document.getElementById('replayGame') ;
